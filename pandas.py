aa1d28cc674dfef5ced276994d53fe16c6655e55
pc aing <christianbayu@google.com>
2023-03-26 13:24:19 +0700

    upload sheet csv

--- pandas.py
+++ pandas.py
@@ -1,3 +1,3 @@
 import pandas as pd
-data = pd.read_excel(io='./data/Mahasiswa.xlsx' ,dtype={'No. NIM':int,'Nama':object,'Jurusan':object,'Kampus':object})
+data = pd.read_excel(io='./data/Mahasiswa.csv' ,dtype={'No. NIM':int,'Nama':object,'Jurusan':object,'Kampus':object})
 print(data)
